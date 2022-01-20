d6dbafc31cfa4ed708fc225cc75176d46fdb9794
BingPow <ebalasca@uvm.edu>
2022-01-20 10:48:21 -0500

    Run with time.sleep() in code

--- simulate.py
+++ simulate.py
@@ -1,8 +1,8 @@
-from time import sleep
+import time
 import pybullet as p
 physicsClient = p.connect(p.GUI)
 for i in range (1,1001):
     print(i)
     p.stepSimulation()
-    time.sleep(5)
+    time.sleep(1/60)
 p.disconnect()
