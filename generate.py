1eaf1e9bccfc1373a5df5861c1d1f8f5dd9eea1a
BingPow <ebalasca@uvm.edu>
2022-01-27 12:28:12 -0500

    Put the stack of 10 boxes in a 5x5 array on the board with three nested for loops.

--- generate.py
+++ generate.py
@@ -1,15 +1,19 @@
 import pyrosim.pyrosim as pyrosim
 pyrosim.Start_SDF("boxes.sdf")
-length = 1
-width = 1
-height = 1
-x = 0
-y = 0
-z = 0.5
-x1 = 1
-y1 = 0
-z1 = 1.5
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x1,y1,z1] , size=[length,width,height])
+perc = 0.9
+for n in range(5):
+    for m in range(5):
+        length = 1
+        width = 1
+        height = 1
+        x = 0
+        y = 0
+        z = 0.5
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x+n,y+m,z] , size=[length,width,height])
+            length *= perc
+            width *= perc
+            height *= perc
+            z += 1
 pyrosim.End()
 
