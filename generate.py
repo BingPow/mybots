f2f0640bb4b9ef7b580fe59bc69a41ab33d8e605
BingPow <ebalasca@uvm.edu>
2022-01-27 12:41:27 -0500

    Return the boxes, to just one signular box, but the sdf file is renamed to world.sdf.

--- generate.py
+++ generate.py
@@ -1,19 +1,11 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("boxes.sdf")
-perc = 0.9
-for n in range(5):
-    for m in range(5):
-        length = 1
-        width = 1
-        height = 1
-        x = 0
-        y = 0
-        z = 0.5
-        for i in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[x+n,y+m,z] , size=[length,width,height])
-            length *= perc
-            width *= perc
-            height *= perc
-            z += 1
+pyrosim.Start_SDF("world.sdf")
+length = 1
+width = 1
+height = 1
+x = 0
+y = 0
+z = 0.5
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 pyrosim.End()
 
