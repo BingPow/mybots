cd4508d70ebd32bc897101ee16c712ffbca04b24
BingPow <ebalasca@uvm.edu>
2022-01-28 12:14:20 -0500

    Made new files world.sdf and body.urdf, they hold the body and world as describe. Adding a joint now between two links and the world and robots are created in functions in generate.py.

--- generate.py
+++ generate.py
@@ -1,18 +1,20 @@
+from pyparsing import pythonStyleComment
 import pyrosim.pyrosim as pyrosim
 length = 1
 width = 1
 height = 1
-x = 0
-y = 0
-z = 0.5
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-    pyrosim.End()
-def Create_Robot():
-    pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.Send_Cube(name="Box", pos=[-5,-5,0.5] , size=[length,width,height])
     pyrosim.End()
 
-Create_World
-Create_Robot
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[length,width,height])
+    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5,0,1.0])
+    pyrosim.Send_Cube(name="Leg", pos=[0,0,0.5], size = [length,width,height])
+    pyrosim.End()
+
+Create_World()
+
+Create_Robot()
