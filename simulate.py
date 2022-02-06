3e87162061af168cb8ccd10833422d67c77ef02b
BingPow <ebalasca@uvm.edu>
2022-02-05 19:40:47 -0500

    Added a sensor to the back leg in the for loop. Pyrosim is imported. We can not tell if the sensors are working yet.

--- simulate.py
+++ simulate.py
@@ -1,15 +1,18 @@
 import time
 import pybullet as p
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
 for i in range (1,1001):
     print(i)
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     time.sleep(1/60)
 p.disconnect()
 
