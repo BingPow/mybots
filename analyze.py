aedf0c9f40682ff11aae1c696d324191a7c44be0
BingPow <ebalasca@uvm.edu>
2022-02-14 09:40:30 -0500

    A lot of changes and didn't commit much in this branch. A lot happened though, Added target angles variables for front leg and back leg, played with variables so that the robot moves forward but it was very challenging. The analyze.py aint nothing too special and was giving me some wacky graphs that weren't the same as the ones put on the reddit wiki for posts, but I knew it had to be right as the length of the targetAngles vector was right.

--- analyze.py
+++ analyze.py
@@ -1,9 +1,13 @@
 import numpy
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
-valueBack = numpy.load('data/backLegSensorValues.npy')
-valueFront = numpy.load('data/frontLegSensorValues.npy')
-matplotlib.pyplot.plot(valueBack,linewidth=2)#linewidth=2, can be used but I think its unneeded
-matplotlib.pyplot.plot(valueFront,linewidth=2)#same here
-matplotlib.pyplot.legend(['Back Leg','Front Leg'])
-matplotlib.pyplot.show()
+#valueBack = numpy.load('data/backLegSensorValues.npy')
+#valueFront = numpy.load('data/frontLegSensorValues.npy')
+#plt.plot(valueBack,linewidth=2)#linewidth=2, can be used but I think its unneeded
+#plt.plot(valueFront,linewidth=2)#same here
+#plt.legend(['Back Leg','Front Leg'])
+#plt.show()
+
+valueAngles = numpy.load('data/targetAngles.npy')
+plt.plot(valueAngles, (numpy.pi/4)*numpy.sin(10*valueAngles))
+plt.show()
--- simulate.py
+++ simulate.py
@@ -1,8 +1,28 @@
+from cmath import sin
 import time
 import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
+import random
+from math import pi
+import matplotlib.pylab as plt
+
+#NUMBER OF STEPS, VERY IMPORTANT for FOR LOOP, SENSORS AND TARGET ANGLES
+steps = 1000
+
+amplitudeBL = pi/4
+frequencyBL = 7
+phaseOffSetBL = pi/4
+amplitudeFL = pi/4
+frequencyFL = 1
+phaseOffSetFL = pi/8
+#amplitude * sin(frequency * i + phaseOffset)
+#targetAngles = numpy.linspace(0,2*pi,steps)
+#numpy.save('data/targetAngles.npy',targetAngles)
+#exit()
+targetAnglesFL = amplitudeBL*numpy.sin(frequencyBL*numpy.linspace(0,2*pi, steps) + phaseOffSetBL)
+targetAnglesBL = amplitudeFL*numpy.sin(frequencyFL*numpy.linspace(0,2*pi, steps) + phaseOffSetFL)
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
@@ -10,23 +30,32 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
-backLegSensorValues = numpy.zeros(1000)
-frontLegSensorValues = numpy.zeros(1000)
+backLegSensorValues = numpy.zeros(steps)
+frontLegSensorValues = numpy.zeros(steps)
 
-for i in range (1000): #should be 1000
+for i in range (steps): #should be 1000
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    #Back Leg
     pyrosim.Set_Motor_For_Joint(
         bodyIndex = robotId,
         jointName = "Torso_BackLeg",
         controlMode = p.POSITION_CONTROL,
-        targetPosition = 0.0,
-        maxForce = 500)
+        targetPosition = targetAnglesBL[i],
+        maxForce = 10)
+    #Front Leg
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robotId,
+        jointName = "Torso_FrontLeg",
+        controlMode = p.POSITION_CONTROL,
+        targetPosition = targetAnglesFL[i],
+        maxForce = 10)
     time.sleep(1/60)
 
 numpy.save('data/backLegSensorValues.npy',backLegSensorValues)
 numpy.save('data/frontLegSensorValues.npy',frontLegSensorValues)
+#numpy.save('data/targetAngles.npy',targetAngles)
 
 p.disconnect()
 
