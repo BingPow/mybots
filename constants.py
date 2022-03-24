eece114755dee7e0c3178d7650530511f24262de
BingPow <ebalasca@uvm.edu>
2022-03-24 12:34:49 -0400

    Standing and moving

--- constants.py
+++ constants.py
@@ -11,18 +11,20 @@ numMotorNeurons = 8
 #usually 1000
 steps = 1000
 
-numberOfGenerations = 1 # could be 10
+numberOfGenerations = 10 # could be 10
 
-populationSize = 1
+populationSize = 10
+
+maxForce = 50
+
+sleep = 1 / 240
+
+motorJointRange = 0.4
 
 #Amplitudes and Front Legs
 amplitude = pi/4
 frequency = 7
 phaseOffSet = pi/4
-
-maxForce = 50
-
-sleep = 1 / 240
 
 length = 1
 width = 1
